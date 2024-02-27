f11895ee12d2b5204a95a5570b84d25701c13557
maxer <max.erkmar@iths.se>
2024-02-27 13:27:34 +0100

    Uppdated the BookResource class

--- src/main/java/se/iths/jakartaeelabb1/resource/BookResource.java
+++ src/main/java/se/iths/jakartaeelabb1/resource/BookResource.java
@@ -1,16 +1,57 @@
 package se.iths.jakartaeelabb1.resource;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import se.iths.jakartaeelabb1.dto.BookDto;
+import se.iths.jakartaeelabb1.dto.Books;
+import se.iths.jakartaeelabb1.entity.Book;
+import se.iths.jakartaeelabb1.repository.BookRepository;
 
-@Path("/hello-world")
+import java.net.URI;
+import java.time.LocalDateTime;
+
+@Path("books")
 public class BookResource {
-    @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    private BookRepository bookRepository;
+
+    public BookResource() {
     }
 
+    @Inject
+    public BookResource(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Books all() {
+        return new Books(
+                bookRepository.all().stream().map(BookDto::map).toList());
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public BookDto one(@PathParam("id") long id){
+        var person = bookRepository.findById(id);
+        if( person == null)
+            throw new NotFoundException("Invalid id " + id);
+        return BookDto.map(person);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    //@Produces(MediaType.APPLICATION_JSON)
+    public Response create(BookDto personDto){
+        //Save to database
+        var p = bookRepository.add(BookDto.map(personDto));
+
+        return Response.created(
+                        //Ask Jakarta application server for hostname and url path
+                        URI.create("http://localhost:8080/api/books/" + p.getId()))
+                .build();
+    }
 
 }

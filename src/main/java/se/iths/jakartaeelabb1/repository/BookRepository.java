76a62b42550868fd3948b561906a4746037d1a63
maxer <max.erkmar@iths.se>
2024-02-27 13:13:22 +0100

    Uppdated the BookRepository class

--- src/main/java/se/iths/jakartaeelabb1/repository/BookRepository.java
+++ src/main/java/se/iths/jakartaeelabb1/repository/BookRepository.java
@@ -1,4 +1,31 @@
 package se.iths.jakartaeelabb1.repository;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import se.iths.jakartaeelabb1.entity.Book;
+
+import java.util.List;
+
+@ApplicationScoped
 public class BookRepository {
+    @PersistenceContext(unitName = "mysql")
+    EntityManager entityManager;
+
+    public List<Book> all() {
+        return entityManager
+                .createQuery("select b from Book b", Book.class)
+                .getResultList();
+    }
+
+    @Transactional
+    public Book add(Book book) {
+        entityManager.persist(book);
+        return book;
+    }
+
+    public Book findById(long id) {
+        return entityManager.find(Book.class, id);
+    }
 }

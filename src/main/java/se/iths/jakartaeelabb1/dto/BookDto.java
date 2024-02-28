bf35af7ef181e2df1c539ccf38ebbde90fa35d12
Alfred <alfredbild@gmail.com>
2024-02-28 11:16:47 +0100

    Add bean annotations in "BookDto"

--- src/main/java/se/iths/jakartaeelabb1/dto/BookDto.java
+++ src/main/java/se/iths/jakartaeelabb1/dto/BookDto.java
@@ -1,8 +1,17 @@
 package se.iths.jakartaeelabb1.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import se.iths.jakartaeelabb1.entity.Book;
 
-public record BookDto(String title, String author, Long id, int year) {
+public record BookDto(
+        @NotBlank(message = "Title cannot be blank")
+        String title,
+        @NotBlank(message = "Author cannot be blank")
+        String author,
+        Long id,
+        @NotNull(message = "Year cannot be null")
+        int year) {
 
     public static BookDto map(Book bookEntity) {
         return new BookDto(bookEntity.getTitle(), bookEntity.getAuthor(), bookEntity.getId(), bookEntity.getYear());

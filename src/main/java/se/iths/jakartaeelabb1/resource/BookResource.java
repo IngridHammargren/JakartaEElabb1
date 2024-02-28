d23438dedc2e272fd9c4fa927d209986cb1f0c5a
maxer <max.erkmar@iths.se>
2024-02-28 11:35:06 +0100

    added response code to delete method

--- src/main/java/se/iths/jakartaeelabb1/resource/BookResource.java
+++ src/main/java/se/iths/jakartaeelabb1/resource/BookResource.java
@@ -42,15 +42,13 @@ public class BookResource {
     //@Produces(MediaType.APPLICATION_JSON)
     public Response create(@Valid BookDto bookDto){
         var b = bookService.add(bookDto);
-        return Response.created(
-                        URI.create("http://localhost:8080/api/books/" + b.getId()))
-                .build();
+        return Response.created(URI.create("http://localhost:8080/api/books/" + b.getId())).build();
     }
     @DELETE
     @Path("{id}")
     public Response delete(@PathParam("id") long id) {
         bookService.delete(id);
-        return Response.noContent().build();
+        return Response.ok().entity("Book with ID " + id + " deleted successfully.").build();
     }
 
 }

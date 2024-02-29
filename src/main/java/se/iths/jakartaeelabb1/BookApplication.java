1caf04763a194a08c8a90c7cf43c41af6c783f19
Hatice Celik <hatice.celik@iths.se>
2024-02-29 10:25:25 +0100

    update pom & url Co-authored-by: Ingrid Hammargren <ingrid.hammargren@iths.se>

--- src/main/java/se/iths/jakartaeelabb1/BookApplication.java
+++ src/main/java/se/iths/jakartaeelabb1/BookApplication.java
@@ -3,7 +3,7 @@ package se.iths.jakartaeelabb1;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("")
 public class BookApplication extends Application {
 
 }
